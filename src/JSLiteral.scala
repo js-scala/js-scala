1f8aa538ca081d52999efe65f9c42145119ef5ca
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2011-09-28 16:05:49 +0200

    It works, but a bit hacky...

--- src/JSLiteral.scala
+++ src/JSLiteral.scala
@@ -17,16 +17,19 @@ trait JSLiteral extends Base with EmbeddedControls {
 
 trait JSLiteralExp extends JSLiteral with BaseExp {
   trait JSLiteral extends Row[Rep]
+
   case class JSLiteralDef(members: List[(String, Exp[Any])]) extends Def[JSLiteral]
-  case class DynamicCall(receiver: Exp[Any], method: String, args: List[Exp[Any]]) extends Def[Any]
-  case class DynamicSelect(receiver: Exp[Any], field: String) extends Def[Any]
+  case class MemberCall(receiver: Exp[Any], method: String, args: List[Exp[Any]]) extends Def[Any]
+  case class MemberSelect(receiver: Exp[Any], field: String) extends Def[Any]
   case class Member(value: Exp[Any]) extends Def[Any]
+  case class JSLiteralSelf(map: scala.collection.mutable.Map[String, Exp[Any]]) extends Exp[JSLiteral]
+
   class JSLiteralOpsImpl(val receiver: Exp[JSLiteral]) extends JSLiteralOps {
     def applyDynamic[T](method: String)(args: Exp[Any]*): Exp[T] =
-      (DynamicCall(receiver, method, args.toList): Exp[Any]).asInstanceOf[Exp[T]]
+      (MemberCall(receiver, method, args.toList): Exp[Any]).asInstanceOf[Exp[T]]
 
     def selectDynamic[T](field: String): Exp[T] =
-      (DynamicSelect(receiver, field): Exp[Any]).asInstanceOf[Exp[T]]
+      (MemberSelect(receiver, field): Exp[Any]).asInstanceOf[Exp[T]]
   }
   implicit def jsLiteralOps(receiver: Exp[JSLiteral]): JSLiteralOps = new JSLiteralOpsImpl(receiver)
   def newJSLiteral(args: (String, Rep[JSLiteral] => (Rep[t] forSome{type t}))*): Exp[JSLiteral] = {
@@ -34,7 +37,9 @@ trait JSLiteralExp extends JSLiteral with BaseExp {
       case Sym(_) => exp
       case _ => Member(exp)
     }
-    val evalArgs = args.toList map { case (name, f) => (name, ensureSym(f(null))) }
+    val map = scala.collection.mutable.Map[String, Exp[Any]]()
+    val evalArgs = args.toList map { case (name, f) => (name, ensureSym(f(JSLiteralSelf(map)))) }
+    evalArgs.foreach(map += _)
     JSLiteralDef(evalArgs)
   }
 }
@@ -43,14 +48,19 @@ trait JSGenLiteral extends JSGenBase {
   val IR: JSLiteralExp
   import IR._
 
+  def getMember(receiver : Exp[Any], member: String) = receiver match {
+    case JSLiteralSelf(map) => quote(map.get(member).get)
+    case _ => quote(receiver) + "." + member
+  }
+
   override def emitNode(sym: Sym[Any], rhs: Def[Any])(implicit stream: PrintWriter) = rhs match {
     case JSLiteralDef(members) => emitValDef(sym,
       members.map({case (name, value) => "'" + name + "' : " + quote(value)}).mkString("{", ",", "}"))
     case Member(value) => emitValDef(sym, quote(value))
-    case DynamicCall(receiver, method, args) =>  emitValDef(sym,
-      quote(receiver) + "." + method + args.map(quote).mkString("(", ",", ")"))
-    case DynamicSelect(receiver, field) =>
-      emitValDef(sym, quote(receiver) + "." + field)
+    case MemberCall(receiver, method, args) =>  emitValDef(sym,
+      getMember(receiver, method) + args.map(quote).mkString("(", ",", ")"))
+    case MemberSelect(receiver, field) => emitValDef(sym,
+      getMember(receiver, field))
     case _ => super.emitNode(sym, rhs)
   }
 }
