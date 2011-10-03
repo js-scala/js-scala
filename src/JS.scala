588d26c5759708fecd19599c3c8b6f484057188d
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2011-10-04 01:03:13 +0200

    Added LiftString to JS so we can use String constants.

--- src/JS.scala
+++ src/JS.scala
@@ -1,6 +1,6 @@
 import scala.virtualization.lms.common._
 
-trait JS extends Arith with Equal with IfThenElse with DynamicBase with JSFunctions with JSLiteral
+trait JS extends Arith with Equal with IfThenElse with LiftString with DynamicBase with JSFunctions with JSLiteral
 
 trait JSExp extends JS with ArithExpOpt with EqualExp with IfThenElseExp with DynamicExp with JSFunctionsExp with JSLiteralExp
 
