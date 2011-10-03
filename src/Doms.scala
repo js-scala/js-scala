5b2c7e7003a9bd21d8a17c160524107469167968
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2011-10-04 01:05:00 +0200

    First steps towards Dom implementation.
    Provided document DOM variable.

---
+++ src/Doms.scala
@@ -0,0 +1,17 @@
+trait Doms extends DynamicBase {
+  val document: DynamicRep
+}
+
+trait DomsExp extends Doms with DynamicExp {
+  case object Document extends Exp[Any]
+  val document = dynamic(Document)
+}
+
+trait GenDoms extends JSGenBase {
+  val IR: DomsExp
+  import IR._
+  override def quote(x: Exp[Any]) : String = x match {
+    case Document => "document"
+    case _ => super.quote(x)
+  }
+}
