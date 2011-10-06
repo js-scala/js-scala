f9a1744a1d599d57b388269256129accc3b08b7f
Nada Amin <namin@alum.mit.edu>
2011-10-06 16:03:14 +0200

    foreach

--- src/JSArrays.scala
+++ src/JSArrays.scala
@@ -9,12 +9,14 @@ trait Arrays extends Base {
     def apply(i: Rep[Int]) = array_apply(a, i)
     def length = array_length(a)
     def update(i: Rep[Int], x: Rep[T]) = array_update(a, i, x)
+    def foreach(block: Rep[T] => Rep[Unit]) = array_foreach(a, block)
   }
 
   def array[T:Manifest](xs: Rep[T]*): Rep[Array[T]]
   def array_apply[T:Manifest](a: Rep[Array[T]], i: Rep[Int]): Rep[T]
   def array_length[T:Manifest](a: Rep[Array[T]]): Rep[Int]
   def array_update[T:Manifest](a: Rep[Array[T]], i: Rep[Int], x: Rep[T]): Rep[Unit]
+  def array_foreach[T:Manifest](a: Rep[Array[T]], block: Rep[T] => Rep[Unit]): Rep[Unit]
   
 }
 
@@ -23,11 +25,32 @@ trait ArraysExp extends Arrays with EffectExp {
   case class ArrayApply[T:Manifest](a: Exp[Array[T]], i: Exp[Int]) extends Def[T]
   case class ArrayLength[T:Manifest](a: Exp[Array[T]]) extends Def[Int]
   case class ArrayUpdate[T:Manifest](a: Exp[Array[T]], i: Exp[Int], x: Exp[T]) extends Def[Unit]
+  case class ArrayForeach[T:Manifest](a: Exp[Array[T]], x: Sym[T], block: Exp[Unit]) extends Def[Unit]
 
-  def array[T:Manifest](xs: Rep[T]*) = reflectEffect(ArrayLiteral(xs.toList), Alloc())
-  def array_apply[T:Manifest](a: Rep[Array[T]], i: Rep[Int]) = ArrayApply(a, i)
-  def array_length[T:Manifest](a: Rep[Array[T]]) = ArrayLength(a)
-  def array_update[T:Manifest](a: Rep[Array[T]], i: Rep[Int], x: Rep[T]) = reflectEffect(ArrayUpdate(a, i, x))
+  def array[T:Manifest](xs: Exp[T]*) = reflectEffect(ArrayLiteral(xs.toList), Alloc())
+  def array_apply[T:Manifest](a: Exp[Array[T]], i: Exp[Int]) = ArrayApply(a, i)
+  def array_length[T:Manifest](a: Exp[Array[T]]) = ArrayLength(a)
+  def array_update[T:Manifest](a: Exp[Array[T]], i: Exp[Int], x: Exp[T]) = reflectEffect(ArrayUpdate(a, i, x))
+  def array_foreach[T:Manifest](a: Exp[Array[T]], block: Exp[T] => Exp[Unit]) = {
+    val x = fresh[T]
+    val b = reifyEffects(block(x))
+    reflectEffect(ArrayForeach(a, x, b), summarizeEffects(b).star)
+  }
+
+  override def syms(e: Any): List[Sym[Any]] = e match {
+    case ArrayForeach(a, x, body) => syms(a):::syms(body)
+    case _ => super.syms(e)
+  }
+
+  override def boundSyms(e: Any): List[Sym[Any]] = e match {
+    case ArrayForeach(a, x, body) => x :: effectSyms(body)
+    case _ => super.boundSyms(e)
+  }
+
+  override def symsFreq(e: Any): List[(Sym[Any], Double)] = e match {
+    case ArrayForeach(a, x, body) => freqNormal(a):::freqHot(body)
+    case _ => super.symsFreq(e)
+  }
 }
 
 trait JSGenArrays extends JSGenEffect {
@@ -39,6 +62,12 @@ trait JSGenArrays extends JSGenEffect {
     case ArrayApply(a, i) => emitValDef(sym, quote(a) + "[" + quote(i) + "]")
     case ArrayLength(a) => emitValDef(sym, quote(a) + ".length")
     case ArrayUpdate(a, i, x) => emitValDef(sym, quote(a) + "[" + quote(i) + "] = " + quote(x))
+    case ArrayForeach(a, x, block) =>
+      stream.println("var " + quote(sym) + "=" + quote(a) + ".forEach(")
+      stream.println("function(" + quote(x) + ",i_,a_){")
+      emitBlock(block)
+      stream.println(quote(getBlockResult(block)))
+      stream.println("})")
     case _ => super.emitNode(sym, rhs)
   }
 }
--- test-src/TestArrays.scala
+++ test-src/TestArrays.scala
@@ -3,11 +3,20 @@ import scala.virtualization.lms.common._
 import java.io.PrintWriter
 import java.io.FileOutputStream
 
-trait Prog1 { this: LiftNumeric with NumericOps with Arrays =>
-  def test(x: Rep[Int]): Rep[Int] = {
+trait Prog { this: LiftNumeric with NumericOps with Arrays =>
+  def test1(x: Rep[Int]): Rep[Int] = {
     val a = array(1, 2, 3)
     a(1) = 4
     a.length + a(1) + x
+  }
+
+  def test2(x: Rep[Int]): Rep[Int] = {
+    val a = array(1, 2, 3)
+    val b = array(4, 5, 6)
+    for (el <- a) {
+      b(0) = el
+    }
+    b(0)
   }
 }
 
@@ -18,12 +27,18 @@ class TestArrays extends FileDiffSuite {
   def testArrays = {
     withOutFile(prefix+"arrays") {
     
-      println("-- begin 1")
-      new Prog1 with LiftNumeric with NumericOpsExpOpt with ArraysExp { self =>
+      new Prog with LiftNumeric with NumericOpsExpOpt with ArraysExp { self =>
         val codegen = new JSGenNumericOps with JSGenArrays { val IR: self.type = self }
-        codegen.emitSource(test _, "main", new PrintWriter(System.out))
+
+	println("-- begin 1")
+        codegen.emitSource(test1 _, "main", new PrintWriter(System.out))
+        println("-- end 1")
+
+	println("-- begin 2")
+        codegen.emitSource(test2 _, "main", new PrintWriter(System.out))
+        println("-- end 2")
+
       }
-      println("-- end 1")
 
     }
     assertFileEqualsCheck(prefix+"arrays")
