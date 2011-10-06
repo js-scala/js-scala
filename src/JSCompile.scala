40df1d0eb636d87b07faaa4e86f99311e6c359c0
Nada Amin <namin@alum.mit.edu>
2011-10-06 15:34:03 +0200

    done replacing arith with numeric ops from core

--- src/JSCompile.scala
+++ src/JSCompile.scala
@@ -127,19 +127,6 @@ trait JSGenIfThenElse extends BaseGenIfThenElse with JSGenEffect { // it's more 
   }
 }
 
-trait JSGenArith extends JSGenBase { // TODO: define a generic one
-  val IR: ArithExp
-  import IR._
-
-  override def emitNode(sym: Sym[Any], rhs: Def[Any])(implicit stream: PrintWriter) = rhs match {
-    case Plus(a,b) =>  emitValDef(sym, "" + quote(a) + "+" + quote(b))
-    case Minus(a,b) => emitValDef(sym, "" + quote(a) + "-" + quote(b))
-    case Times(a,b) => emitValDef(sym, "" + quote(a) + "*" + quote(b))
-    case Div(a,b) =>   emitValDef(sym, "" + quote(a) + "/" + quote(b))
-    case _ => super.emitNode(sym, rhs)
-  }
-} 
-
 trait JSGenNumericOps extends JSGenBase {
   val IR: NumericOpsExp
   import IR._
