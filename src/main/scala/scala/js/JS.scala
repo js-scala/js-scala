09230bff8f8ad618b445af93deaea806758ab634
Nada Amin <namin@alum.mit.edu>
2011-10-07 16:47:47 +0200

    added arrays to default JS

--- src/main/scala/scala/js/JS.scala
+++ src/main/scala/scala/js/JS.scala
@@ -2,10 +2,10 @@ package scala.js
 
 import scala.virtualization.lms.common._
 
-trait JS extends LiftNumeric with NumericOps with Equal with IfThenElse with LiftString with DynamicBase with JSFunctions with JSLiteral
+trait JS extends LiftNumeric with NumericOps with Equal with IfThenElse with LiftString with DynamicBase with Arrays with JSFunctions with JSLiteral
 
-trait JSExp extends JS with NumericOpsExpOpt with EqualExp with IfThenElseExp with DynamicExp with JSFunctionsExp with JSLiteralExp
+trait JSExp extends JS with NumericOpsExpOpt with EqualExp with IfThenElseExp with DynamicExp with ArraysExp with JSFunctionsExp with JSLiteralExp
 
-trait JSGen extends JSGenNumericOps with JSGenEqual with JSGenIfThenElse with JSGenDynamic with JSGenFunctions with JSGenLiteral with JSGenEffect {
+trait JSGen extends JSGenNumericOps with JSGenEqual with JSGenIfThenElse with JSGenDynamic with JSGenArrays with JSGenFunctions with JSGenLiteral {
   val IR: JSExp
 }
