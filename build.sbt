07986facd02162c11ebbac026199af0de36049b9
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2011-10-28 16:36:40 +0200

    Added initial example for websocket taken from jetty.
    We are using jetty and xsbt's web plugin here. In
    order to start the sample just run container:start
    in sbt's console and then access http://localhost:8080.
    
    Signed-off-by: Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>

--- build.sbt
+++ build.sbt
@@ -28,11 +28,19 @@ resolvers ++= Seq(
 
 libraryDependencies ++= Seq(
     "org.scalatest" % "scalatest_2.10.0-virtualized-SNAPSHOT" % "1.6.1-SNAPSHOT" % "test",
-    "scala" % "virtualization-lms-core_2.10.0-virtualized-SNAPSHOT" % "0.1")
-    
+    "scala" % "virtualization-lms-core_2.10.0-virtualized-SNAPSHOT" % "0.1",
+    "org.eclipse.jetty" % "jetty-webapp" % "8.0.1.v20110908" % "container",
+    "org.eclipse.jetty" % "jetty-websocket" % "8.0.1.v20110908",
+    "org.eclipse.jetty" % "jetty-servlet" % "8.0.1.v20110908",
+    "javax.servlet" % "servlet-api" % "2.5" % "provided->default")
+
+
 //--- End of Dependencies
 
 scalacOptions ++= Seq("-deprecation", "-unchecked", "-Xexperimental")
 
 //Our tests are not threadsafe so disabling parallel execution for now
 parallelExecution in Test := false
+
+//import web plugin keys
+seq(webSettings :_*)
---
+++ src/main/java/scala/js/actor/WebSocketChatServlet.java
@@ -0,0 +1,78 @@
+package scala.js.actor;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.util.TypeUtil;
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.websocket.WebSocket;
+import org.eclipse.jetty.websocket.WebSocketServlet;
+
+public class WebSocketChatServlet extends WebSocketServlet
+{
+    private final Set<ChatWebSocket> _members = new CopyOnWriteArraySet<ChatWebSocket>();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws javax.servlet.ServletException ,IOException
+    {
+        getServletContext().getNamedDispatcher("default").forward(request,response);
+    };
+
+    public WebSocket doWebSocketConnect(HttpServletRequest request, String protocol)
+    {
+        return new ChatWebSocket();
+    }
+
+    /* ------------------------------------------------------------ */
+    /* ------------------------------------------------------------ */
+    class ChatWebSocket implements WebSocket.OnTextMessage
+    {
+        Connection _connection;
+
+        public void onOpen(Connection connection)
+        {
+            // Log.info(this+" onConnect");
+            _connection=connection;
+            _members.add(this);
+        }
+
+        public void onMessage(byte frame, byte[] data,int offset, int length)
+        {
+            // Log.info(this+" onMessage: "+TypeUtil.toHexString(data,offset,length));
+        }
+
+        public void onMessage(String data)
+        {
+            if (data.indexOf("disconnect")>=0)
+                _connection.disconnect();
+            else
+            {
+                // Log.info(this+" onMessage: "+data);
+                for (ChatWebSocket member : _members)
+                {
+                    try
+                    {
+                        member._connection.sendMessage(data);
+                    }
+                    catch(IOException e)
+                    {
+                        Log.warn(e);
+                    }
+                }
+            }
+        }
+
+        public void onClose(int code, String message)
+        {
+            // Log.info(this+" onDisconnect");
+            _members.remove(this);
+        }
+
+    }
+}
