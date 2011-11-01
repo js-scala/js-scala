f73d5239d126be416562c9df516e686f4391e6a2
Nada Amin <namin@alum.mit.edu>
2011-11-01 18:22:51 +0100

    Translated the whole servlet to Scala. The demo works again. The members field need to be part of the whole servlet, not just the transient instance answering one request.

--- src/main/scala/scala/js/actor/JettyChat.scala
+++ src/main/scala/scala/js/actor/JettyChat.scala
@@ -1,39 +1,52 @@
 package scala.js.actor
 
-import org.eclipse.jetty.websocket.WebSocket.Connection
 import java.util.concurrent.CopyOnWriteArraySet
+
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+import org.eclipse.jetty.websocket.WebSocket
+import org.eclipse.jetty.websocket.WebSocket.Connection
+import org.eclipse.jetty.websocket.WebSocketServlet
+
 import scala.collection.JavaConversions._
 
-class ChatWebSocket extends OnTextMessage {
+class WebSocketChatServlet extends WebSocketServlet {
+  private val _members = new CopyOnWriteArraySet[ChatWebSocket]();
 
-  val _members = new CopyOnWriteArraySet[ChatWebSocket]();
+  override protected def doGet(request: HttpServletRequest, response: HttpServletResponse) {
+    getServletContext().getNamedDispatcher("default").forward(request, response)
+  }
 
-  var _connection : Connection = null;
+  override def doWebSocketConnect(request: HttpServletRequest, protocol: String) : WebSocket = new ChatWebSocket()
 
-  def onOpen(connection: Connection) {
+  class ChatWebSocket extends OnTextMessage {
+    private var _connection : Connection = null;
+
+    def onOpen(connection: Connection) {
       // Log.info(this+" onConnect");
       _connection=connection;
       _members.add(this);
-  }
+    }
 
-  def onMessage(frame: Byte, data: Array[Byte], offset: Int, length: Int) {
+    def onMessage(frame: Byte, data: Array[Byte], offset: Int, length: Int) {
       // Log.info(this+" onMessage: "+TypeUtil.toHexString(data,offset,length));
-  }
+    }
 
-  def onMessage(data: String) {
+    def onMessage(data: String) {
       if (data.indexOf("disconnect")>=0)
-          _connection.disconnect();
+        _connection.disconnect();
       else {
-          // Log.info(this+" onMessage: "+data);
-          for (member <- _members) {
-                  member._connection.sendMessage(data);
-          }
+        // Log.info(this+" onMessage: "+data);
+        for (member <- _members) {
+          member._connection.sendMessage(data);
+        }
       }
-  }
+    }
 
-  def onClose(code: Int, message: String) {
+    def onClose(code: Int, message: String) {
       // Log.info(this+" onDisconnect");
       _members.remove(this);
+    }
   }
-
 }
