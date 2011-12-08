216b27ebbace685042eff998656223c6f4279e2f
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2011-12-08 14:14:40 +0100

    Fixed bug in Cell.js when set(undefined) is called.
    Cell can receive undefined value and then the logic breaks.

--- examples/ajax/Cell.js
+++ examples/ajax/Cell.js
@@ -1,22 +1,23 @@
 function spawn(b) {
-  console.log("spawning " + b)
   b()
 }
 function Cell() {
   this.value = undefined
+  this.isDefined = false
   this.queue = []
   this.get = function (k) {
-    if (this.value != undefined) {
+    if (this.isDefined) {
       k(this.value)
     } else {
       this.queue.push(k)
     }
   }
   this.set = function (v) {
-    if (this.value != undefined) {
+    if (this.isDefined) {
       throw "can't set value twice"
     } else {
       this.value = v
+      this.isDefined = true
       this.queue.forEach(function (f) {
         spawn(function () { f(v) })
       })
