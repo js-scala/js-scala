57a400ba0a064bcf5bc517c72837d559f56b6720
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2011-12-13 17:48:00 +0100

    Added casts checked at runtime.

--- src/main/scala/scala/js/Casts.scala
+++ src/main/scala/scala/js/Casts.scala
@@ -2,6 +2,8 @@ package scala.js
 
 import scala.virtualization.lms.common._
 
+import java.io.PrintWriter
+
 trait Casts extends Base {
   trait AsRep {
     def as[T: Manifest]: Rep[T]
@@ -9,4 +11,32 @@ trait Casts extends Base {
   implicit def asRep(x: Rep[_]): AsRep = new AsRep {
     def as[T: Manifest]: Rep[T] = x.asInstanceOf[Rep[T]]
   }
+}
+
+trait CastsCheckedExp extends Casts with EffectExp {
+  case class Cast[T](x: Rep[_], m: Manifest[T]) extends Def[T]
+  override implicit def asRep(x: Rep[_]): AsRep = new AsRep {
+    def as[T: Manifest]: Rep[T] = reflectEffect(Cast[T](x, implicitly[Manifest[T]]))
+  }
+}
+
+trait GenCastChecked extends JSGenEffect {
+  val IR: CastsCheckedExp
+  import IR._
+  
+  override def emitNode(sym: Sym[Any], rhs: Def[Any])(implicit stream: PrintWriter) = rhs match {
+    case Cast(x, m) =>
+      emitValDef(sym, quote(x))
+      conformsCheck(quote(sym), m)
+    case _ => super.emitNode(sym, rhs)
+  }
+  def conformsCheck(v: String, m: Manifest[_])(implicit stream: PrintWriter): Unit = m match {
+    case m: scala.reflect.RefinedManifest[_] =>
+      m.fields foreach { case (name, manifest) => conformsFieldCheck(v, name, manifest) }
+    case _ => println("Can't generate check for " + m + " with of type " + m.getClass)
+  }
+  def conformsFieldCheck(v: String, f: String, m: Manifest[_])(implicit stream: PrintWriter): Unit = {
+    stream.println("""if (!("%1$s" in %2$s)) throw "%1$s is not defined in " + %2$s;""".format(f, v))
+  }
+
 }
---
+++ src/test/scala/scala/js/TestCasts.scala
@@ -0,0 +1,44 @@
+package scala.js
+
+import scala.virtualization.lms.common._
+
+import java.io.PrintWriter
+import java.io.FileOutputStream
+
+trait RefinementCastProg { this: Casts =>
+  def test(x: Rep[Any]): Rep[Any] = {
+    x.as[{val foo: String; val bar: Int}]
+  }
+}
+
+trait JSLiteralCastProg { this: JS with Casts =>
+  def test(x: Rep[Any]): Rep[Any] = {
+    x.as[JSLiteral {val foo: String; val bar: Int}]
+  }
+}
+
+class TestCasts extends FileDiffSuite {
+
+  val prefix = "test-out/"
+
+  def testRefinement = {
+    withOutFile(prefix+"refinement_cast") {
+      new RefinementCastProg with CastsCheckedExp { self =>
+        val codegen = new GenCastChecked { val IR: self.type = self }
+        codegen.emitSource(test _, "main", new PrintWriter(System.out))
+      }
+    }
+    assertFileEqualsCheck(prefix+"refinement_cast")
+  }
+
+  def testJSLiteral = {
+    withOutFile(prefix+"jsliteral_cast") {
+      new JSLiteralCastProg with JSExp with CastsCheckedExp { self =>
+        val codegen = new GenCastChecked { val IR: self.type = self }
+        codegen.emitSource(test _, "main", new PrintWriter(System.out))
+      }
+    }
+    assertFileEqualsCheck(prefix+"jsliteral_cast")
+  }
+
+}
