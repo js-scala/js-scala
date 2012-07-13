3ee6cc0c43e611fbfdb10e9cb95716c2e72c3cc1
Nada Amin <nada.amin@epfl.ch>
2012-07-13 11:25:06 +0200

    cleanup main

--- examples/actors/jsworker.js
+++
@@ -1,9 +0,0 @@
-var connections = 0; // count active connections
-self.addEventListener("connect", function (e) {
-    var port = e.ports[0];
-    connections++;
-    port.addEventListener("message", function (e) {
-	port.postMessage("Hello " + e.data + " (port #" + connections + ")");
-    }, false);
-    port.start();
-}, false);
--- src/main/scala/scala/js/Main.scala
+++ src/main/scala/scala/js/Main.scala
@@ -2,75 +2,12 @@ package scala.js
 
 import scala.virtualization.lms.common._
 
-import java.io.PrintWriter
-import java.io.FileOutputStream
-
-trait Print extends Base {
-  implicit def unit(s: String): Rep[String]
-  def print(s: Rep[Any]): Rep[Unit]
-}
-
-trait PrintExp extends Print with EffectExp {
-  implicit def unit(s: String): Rep[String] = Const(s)
-  case class Print(s: Rep[Any]) extends Def[Unit]
-  def print(s: Rep[Any]) = reflectEffect(Print(s))
-}
-
-trait ScalaGenPrint extends ScalaGenEffect {
-  val IR: PrintExp
-  import IR._
-  
-  override def emitNode(sym: Sym[Any], rhs: Def[Any]) = rhs match {
-    case Print(s) =>  emitValDef(sym, "println(" + quote(s) + ")")
-    case _ => super.emitNode(sym, rhs)
-  }
-}
-
-trait JSGenPrint extends JSGenEffect {
-  val IR: PrintExp
-  import IR._
-  
-  // TODO: should have a function for this
-  override def emitNode(sym: Sym[Any], rhs: Def[Any]) = rhs match {
-    case Print(s) =>  emitValDef(sym, "document.body.appendChild(document.createElement(\"div\"))"+
-        ".appendChild(document.createTextNode("+quote(s)+"))")
-    case _ => super.emitNode(sym, rhs)
-  }
-}
-
-trait FunProg { this: JSFunctions =>
-  def test(x: Rep[Any]): Rep[Any] = {
-    val id = fun { x : Rep[Any] => x }
-    id.apply(x)
-  }
-}
-
-trait SomeProg { this: JS =>
-  def test(x: Rep[Any]): Rep[Any] = {
-    lazy val id: Rep[Any => Any] = fun { x : Rep[Any] =>
-      dynamic(x).foo.bar()
-      id.apply(x)
-    }
-    id.apply(x)
-  }
-}
-
 object Main extends App {
-  new FunProg with JSFunctionsExp { self =>
-    val codegen = new JSGenFunctions { val IR: self.type = self }
-    codegen.emitSource(test _, "main", new PrintWriter(System.out))
-  }
-
-  new SomeProg with JSExp { self =>
-    val codegen = new JSGen { val IR: self.type = self }
-    codegen.emitSource(test _, "main", new PrintWriter(System.out))
-  }
-
-  Koch.writeHtml("koch.html")
+  Koch.writeHtml("examples/canvas/koch_.html")
   if (args.contains("graphics"))
     Koch.run()
   Birds.writeJs("examples/birds/Bird_.js")
   Twitter.writeJs("examples/ajax/twitter_.js")
 
-  D3.writeHtml("d3.html")
+  D3.writeHtml("examples/d3/d3_.html")
 }
