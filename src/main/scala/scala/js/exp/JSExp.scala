30bfb0e6e2d4216c6f1d699d6d5c6b186e069673
Julien Richard-Foy <julien@richard-foy.fr>
2013-05-30 12:51:26 +0200

    Add DomExpOpt to JSExpOpt

--- src/main/scala/scala/js/exp/JSExp.scala
+++ src/main/scala/scala/js/exp/JSExp.scala
@@ -1,8 +1,9 @@
 package scala.js.exp
 
 import scala.js.language.JS
+import scala.js.exp.dom.DomExpOpt
 
 trait JSExp extends JS with JsScalaExp with DynamicsExp with ArraysExp
   with RegExpsExp with OptionOpsExp
 
-trait JSExpOpt extends JSExp with JsScalaExpOpt
+trait JSExpOpt extends JSExp with JsScalaExpOpt with DomExpOpt
