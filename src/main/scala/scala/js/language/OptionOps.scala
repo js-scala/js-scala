1693eeecaacc25b522d720cdba61f40ccfb217b2
Rudi <rtuvache@balistre.irisa.fr>
2013-06-05 15:54:50 +0200

    option-getorelse made with fold (#3)

--- src/main/scala/scala/js/language/OptionOps.scala
+++ src/main/scala/scala/js/language/OptionOps.scala
@@ -16,5 +16,6 @@ trait OptionOps extends Base {
     def flatMap[B : Manifest](f: Rep[A] => Rep[Option[B]]): Rep[Option[B]]
     def isEmpty: Rep[Boolean]
     def fold[B : Manifest](none: => Rep[B], some: Rep[A] => Rep[B]): Rep[B]
+    def getOrElse[B >: A : Manifest](default: => Rep[B]): Rep[B]
   }
 }
